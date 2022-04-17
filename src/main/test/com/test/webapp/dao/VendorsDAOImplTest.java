de34eeff9e30b3d83904aea26310e45065c85a84
rilialov <ilialovrustem@gmail.com>
2022-04-17 16:20:01 +0300

    Added tests for VendorsDAOImpl.

---
+++ src/main/test/com/test/webapp/dao/VendorsDAOImplTest.java
@@ -0,0 +1,69 @@
+package com.test.webapp.dao;
+
+import com.test.webapp.entity.Vendor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class VendorsDAOImplTest {
+
+    private VendorsDAOImpl vendorsDAO;
+
+    @BeforeEach
+    void init() {
+        vendorsDAO = new VendorsDAOImpl();
+    }
+
+    @Test
+    void getById() {
+        Vendor vendor = vendorsDAO.getById(1L);
+
+        Assertions.assertNotNull(vendor);
+    }
+
+    @Test
+    void getAll() {
+        List<Vendor> vendors = vendorsDAO.getAll();
+
+        Assertions.assertNotNull(vendors);
+        Assertions.assertTrue(vendors.size() > 0);
+    }
+
+    @Test
+    void create() {
+        Vendor vendor = new Vendor();
+        vendor.setVendorName("TestVendor");
+        Vendor created = vendorsDAO.create(vendor);
+
+        vendorsDAO.delete(created);
+        Assertions.assertNotNull(created);
+    }
+
+    @Test
+    void update() {
+        Vendor vendor = new Vendor();
+        vendor.setVendorName("TestVendor");
+        Vendor created = vendorsDAO.create(vendor);
+
+        created.setVendorName("ChangedVendor");
+        vendorsDAO.update(created);
+
+        Vendor updated = vendorsDAO.getById(created.getId());
+        vendorsDAO.delete(updated);
+        Assertions.assertEquals("ChangedVendor", updated.getVendorName());
+    }
+
+    @Test
+    void delete() {
+        Vendor vendor = new Vendor();
+        vendor.setVendorName("TestVendor");
+        Vendor created = vendorsDAO.create(vendor);
+
+        vendorsDAO.delete(created);
+
+        Vendor deleted = vendorsDAO.getById(created.getId());
+        Assertions.assertNull(deleted);
+    }
+}
